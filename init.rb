832673baa8a2ad148249b6e0abed86cbe21ab072
Leandro Nunes dos Santos <leandronunes@safernet.org.br>
2008-11-27 18:05:12 -0300

    adapting plugin for rails 2.2 version

--- init.rb
+++ init.rb
@@ -3,29 +3,27 @@ require 'design'
 require 'acts_as_design'
 require 'acts_as_design_block'
 
-Dependencies.load_once_paths.delete(lib_path)
-
 # extra directories for design controllers organization 
 design_blocks_controller_paths = DesignConfiguration.design_blocks_controller_path
 design_blocks_controller_paths.each do |item|
-  Dependencies.load_paths << item
   $LOAD_PATH << item
-  config.controller_paths << item
+  ActiveSupport::Dependencies.load_paths << item
+  ActiveSupport::Dependencies.load_once_paths.delete(item)
 end
 
 # extra directories for design helpers organization 
 design_blocks_helper_paths = DesignConfiguration.design_blocks_helper_path
 design_blocks_helper_paths.each do |item|
-  Dependencies.load_paths << item
   $LOAD_PATH << item
+  ActiveSupport::Dependencies.load_paths << item
+  ActiveSupport::Dependencies.load_once_paths.delete(item)
 end
-
-
 
 # extra directories for design model organization 
 design_blocks_model_paths = DesignConfiguration.design_blocks_model_path
 design_blocks_model_paths.each do |item|
-  Dependencies.load_paths << item
   $LOAD_PATH << item
+  ActiveSupport::Dependencies.load_paths << item
+  ActiveSupport::Dependencies.load_once_paths.delete(item)
 end
 
